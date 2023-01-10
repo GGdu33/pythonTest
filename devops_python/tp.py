7408e7faea5f1a5ba3cbab33f1216e905e9e7f3f
Bourguet <jeremy.bourguet@ynov.com>
2023-01-10 12:20:43 +0100

    Ajout du dossier

---
+++ devops_python/tp.py
@@ -0,0 +1,11 @@
+import unittest
+
+class SimpleMath:
+    @staticmethod
+    def addition(a, b):
+        return a + b
+
+class TestSimpleMath(unittest.TestCase):
+    def test_addition(self):
+        result = SimpleMath.addition(1, 2)
+        self.assertEqual(result, 3)

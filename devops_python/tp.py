166efcee09ec14ca402fe620d1b8ee4a6a5c0eb2
Bourguet <jeremy.bourguet@ynov.com>
2023-01-12 14:19:17 +0100

    Ajout de la fonction soustraction et modification du fichier github-actions

--- devops_python/tp.py
+++ devops_python/tp.py
@@ -5,7 +5,14 @@ class SimpleMath:
     def addition(a, b):
         return a + b
 
+    def soustraction(a,b):
+        return a - b
+
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         result = SimpleMath.addition(1, 2)
+        self.assertEqual(result, 3)
+
+    def test_soustraction(self):
+        result = SimpleMath.soustraction(4,2)
         self.assertEqual(result, 3)
